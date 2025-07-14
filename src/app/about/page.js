9d87b69e0af080a6824fa4a467d2f7bdc7c6b12f
dsacco <dsacco@shipmatrix.com>
2025-07-14 13:30:46 -0400

    Content on about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -15,6 +15,14 @@ export default function AboutPage() {
           </h1>
         </div>
       </ParallaxCard>
+      <section>
+        <h1>Our Roots-</h1>
+      <p>P&S Contracting and Landscape was created on the principle that our quality landscape designs would speak for itself and landscaping maintenance dependability is vital. Furthermore, our original and a unique approach to providing every client with these landscaping services is something we are very proud of. We cater to our landscaping clients and their projects as diligently as we would if they were our own home or business. Also offering other essential services such as softscaping and mulching, we are able to make your property immaculate and welcome to all. </p>
+      </section>
+      <section>
+        <h1>Our Goals-</h1>
+      <p>When you're in need of having your business' landscaping updated and modernized we are the best company in Westmoreland County, PA. Additionally, we have the necessary experience to quickly and efficiently service your home's current landscaping layout and/or work with you hand in hand with you entirely to guarantee your custom landscape design is completely satisfactory to you. Being licensed, bonded and insured, you can take solace in the fact that all of your landscaping services are being addressed by true professionals. We offer our very thorough and outstanding landscaping services at an unbeatable bargain. We also offer free estimates and proposals! As a well established, family owned and operated business that always contributes to our community, we happily offer competitive rates and top notch landscaping services. If you have any questions or are ready to schedule your initial appointment, please contact us.</p>
+      </section>
     </div>
   );
 }
