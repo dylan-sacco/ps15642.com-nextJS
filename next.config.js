21327f88000e0d12ae00d133cfee5b691ea12d0c
dsacco <dylan.sacco@icloud.com>
2026-04-01 08:40:51 -0400

    Increase body size for api calls

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_APP_VERSION: packageJson.version,
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '500mb',
+    },
+  },
 };
 
 module.exports = nextConfig;
